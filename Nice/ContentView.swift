5fd5a25f3c01789131ed76f3dcc460dfdfb0a38d
Iwan Teplyakov <iwan@kitcast.tv>
2021-12-14 12:37:31 +0200

    Initial Commit

---
+++ Nice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nice
+//
+//  Created by Iwan Teplyakov on 12/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nice/NiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceApp.swift
+//  Nice
+//
+//  Created by Iwan Teplyakov on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct NiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
